7512714a5ddeb13484cbbd251b6bc4cd7fa83dc9
myapos <myapos@yahoo.com>
2016-12-25 13:50:06 +0200

    added fullscreen detection in Mozzilla and chrome, status display

--- js/my.js
+++ js/my.js
@@ -1,46 +1,115 @@
 let movies={
 "1": "https://bigcdn.ia3el9bj6x2jcckb4mr8.com/cdnfast1/luq4w7gru3ixexzw6vqlpiuir2r24tumzdb4lxrs36hlp3i4dz7mkft6bvia/low.mp4",
-"2": "https://bigcdn.ia3el9bj6x2jcckb4mr8.com/cdnfast1/luq4wpfai7ixexzw6vs3f7u6rbcl2icqtiouyi7g5bijbcsnotv7isrrp5qq/normal.mp4",
+"2": "https://bigcdn.ia3el9bj6x2jcckb4mr8.com/cdnfast1/luq4wpfai7ixexzw6vs3f7u6rbcl2icqtiouyi7g5b2zefvqfnb43uygmo5q/normal.mp4",
 "3": "https://bigcdn.ia3el9bj6x2jcckb4mr8.com/cdnfast1/luq4qyjhghixexzw6uw3fj4g26jrpvezpnhpl5uj6lzko3vmy3ulb5jay5ba/normal.mp4"
 };
 //console.log(movies);
-let videoplayer =  this.document.getElementById("myMoviePlayer");
+var videoplayer =  document.getElementById("myMoviePlayer");
 var paused = 0;
 var subsAlreadyLoaded = 0;
-const init = () => {
-	let videoplayer =  this.document.getElementById("myMoviePlayer");
+var stateFC = false;
+var browser;
+let nav = navigator.userAgent;
+browser = nav.match(/Chromium/g) || nav.match(/Chrome/g) || nav.match(/Firefox/g);
+console.log(browser);
+var status = document.getElementById('status');
 
+/*AJAX request example*/
+loadDoc = (url) => {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("status").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+const init = () => {
+	let videoplayer =  document.getElementById("myMoviePlayer");
 	videoplayer.addEventListener("loadstart", () => {
 	    let vidplayer = document.getElementById("myMoviePlayer");
 	    vidplayer.style.backgroundImage = "url('../../stream_movie_video_player/images/loading2.gif')";
+	    status.innerHTML = "Loading movie.................";
 	});
 
 	videoplayer.addEventListener("canplay", () => {
 	    let vidplayer = document.getElementById("myMoviePlayer");
 	    vidplayer.style.backgroundImage = "url('')";
+	    status.innerHTML = "Playing now.................";
 	});
 	
-	videoplayer.src = movies["1"];
+	let loadFullScreenEvent="fullscreenchange";
+	if(browser[0] == "Chromium" || browser[0] == "Chrome"){
+		loadFullScreenEvent = "webkitfullscreenchange";
+		videoplayer.addEventListener(loadFullScreenEvent, () => {
+		stateFC = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
+
+		if (stateFC) {
+		  alert("Full screen enabled");
+		  console.log("Full screen enabled");
+		}
+	});
+	}
+	else if(browser[0] == "Firefox"){
+		loadFullScreenEvent = "mozfullscreenchange";
+		//Fix for firefox http://stackoverflow.com/questions/12284198/javascript-mozfullscreenchange-on-firefox
+		//adding event listener to document not to the element
+		document.addEventListener(loadFullScreenEvent, ()=> {
+			console.log("added mozfullscreenchange to document");
+			stateFC = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
+
+			if (stateFC) {
+			  alert("Full screen enabled");
+			  console.log("Full screen enabled");
+			}
+		})
+	}
+
+/*Style dynamically status info*/
+
+var rect = videoplayer.getBoundingClientRect();
+console.log("styling status:", rect.top, rect.right, rect.bottom, rect.left);
+
+
+
+
+videoplayer.src = movies["1"];
 
 }
 
 const selectMovie =() => {
-	let selection = this.document.getElementById("selectBox").value;
+	let selection = document.getElementById("selectBox").value;
 	console.log(selection);
-	let videoplayer =  this.document.getElementById("myMoviePlayer");
+	let videoplayer =  document.getElementById("myMoviePlayer");
 	loadsubs(videoplayer,selection);
 	videoplayer.src = movies[selection];
 }
 
 const pauseMovie = () => {
-	let videoplayer =  this.document.getElementById("myMoviePlayer");
+	var videoplayer =  document.getElementById("myMoviePlayer");
+	
 	if(!paused){
 		videoplayer.pause(); 
 		paused = 1;
+		//videoplayer.style.backgroundImage = "url('../../stream_movie_video_player/images/pause.png')"
+		status.innerHTML = "Paused.................";
+/*		setTimeout( () => 
+			{ 
+				status.innerHTML = "";
+		}, 3000);*/
 	}
 	else{
 		videoplayer.play(); 
 		paused = 0;
+		//videoplayer.style.backgroundImage = "url('../../stream_movie_video_player/images/loading2.gif')";
+		status.innerHTML = "Playing now.................";
+/*		setTimeout( () => 
+			{ 
+				status.innerHTML = "";
+		}, 3000);*/
+
 	}
 }
 
